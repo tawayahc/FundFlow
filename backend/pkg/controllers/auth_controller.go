056600a460b0f35a64186dbf2444ffc3e1a1b5f9
MawinS <mawin.sric@mail.kmutt.ac.th>
2024-11-01 22:57:40 +0700

    refactor: :zap: extract auth_controller to auth_service

--- backend/pkg/controllers/auth_controller.go
+++ backend/pkg/controllers/auth_controller.go
@@ -1,15 +1,11 @@
 package controllers
 
 import (
-	"book-management-system/pkg/config"
 	"book-management-system/pkg/models"
-	"book-management-system/pkg/utils"
+	"book-management-system/pkg/services"
 	"net/http"
-	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/golang-jwt/jwt/v5" // Correct import path
-	"golang.org/x/crypto/bcrypt"
 )
 
 // Register a new user (with email)
@@ -20,35 +16,9 @@ func Register(c *gin.Context) {
 		return
 	}
 
-	// Hash the password
-	hashedPassword, err := utils.HashPassword(creds.Password)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error hashing password"})
-		return
-	}
-
-	// Start a transaction
-	tx := config.DB.Begin()
-
-	// Create a new user record for authentication
-	userAuth := models.Authentication{Username: creds.Username, Password: string(hashedPassword)}
-	if err := tx.Create(&userAuth).Error; err != nil {
-		tx.Rollback() // Rollback the transaction in case of error
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Username already exists"})
-		return
-	}
-
-	// Create a new user profile
-	userProfile := models.UserProfile{Email: creds.Email, AuthID: userAuth.AuthID} // Assuming there's a foreign key
-	if err := tx.Create(&userProfile).Error; err != nil {
-		tx.Rollback() // Rollback the transaction in case of error
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Email already exists"})
-		return
-	}
-
-	// Commit the transaction
-	if err := tx.Commit().Error; err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to complete transaction"})
+	// Create a new user
+	if err := services.CreateUser(creds); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
@@ -63,42 +33,15 @@ func Login(c *gin.Context) {
 		return
 	}
 
-	// Fetch user based on username
-	var user models.Authentication
-	result := config.DB.Where("username = ?", creds.Username).First(&user)
-
-	// Check if user exists and handle failed login attempts
-	if result.Error != nil {
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
+	// Validate user credentials
+	user, err := services.ValidateUserCredentials(creds)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
 		return
 	}
-
-	// Validate password
-	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(creds.Password)); err != nil {
-		// Increment failed login attempts
-		user.FailedAttempts++
-		config.DB.Save(&user)
-		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
-		return
-	}
-
-	// Reset failed login attempts upon successful login
-	user.FailedAttempts = 0
-	user.LastLogin = time.Now() // Update last login timestamp
-	config.DB.Save(&user)
 
 	// Generate JWT token
-	expirationTime := time.Now().Add(24 * time.Hour)
-	claims := &models.Claims{
-		Username: creds.Username,
-		RegisteredClaims: jwt.RegisteredClaims{
-			ExpiresAt: jwt.NewNumericDate(expirationTime),
-		},
-	}
-
-	// Sign the token
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	tokenString, err := token.SignedString([]byte(config.GetEnv("JWT_SECRET")))
+	tokenString, err := services.GenerateJWTToken(user.Username)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error generating token"})
 		return
