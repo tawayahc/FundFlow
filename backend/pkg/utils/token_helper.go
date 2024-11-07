12fdfd355dc890146bce117cf7ae8fa50883626f
MawinS <mawin.sric@mail.kmutt.ac.th>
2024-11-07 11:48:53 +0700

    feat: add change email path

---
+++ backend/pkg/utils/token_helper.go
@@ -0,0 +1,24 @@
+package utils
+
+import (
+	"errors"
+	"fundflow/pkg/models"
+	"strings"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+// ExtractUsernameFromToken extracts the username from the JWT token
+func ExtractUsernameFromToken(authHeader string) (string, error) {
+	// The Authorization header should contain "Bearer <token>"
+	tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+
+	// Parse the JWT token without validation
+	claims := &models.Claims{}
+	_, _, err := new(jwt.Parser).ParseUnverified(tokenString, claims)
+	if err != nil {
+		return "", errors.New("invalid token")
+	}
+
+	return claims.Username, nil
+}
