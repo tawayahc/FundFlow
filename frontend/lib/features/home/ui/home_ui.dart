2db95e5dde2651dc346bde3fd45e0de9f3cbfe42
Sarun Khumthai <73182667+OATZYH@users.noreply.github.com>
2024-11-07 21:35:22 +0700

    fix : Frontend homepage
    * feat: Change home page follow Figma
    
    * style: make it more likely to figma
    
    * refactor: Refactor code and change UI of profile page
    
    init profile sectoin and refactor bloc file
    
    * style: Apply Layout
    
    layout margin
    
    * style: Decoration home
    
    change color change style
    
    * feat: Create add feature
    
    create add_bank, add_category
    
    * refactor: Rename file
    
    from expense to category
    
    * style: Change background color
    
    Change background color to real white
    
    ---------
    
    Co-authored-by: FlukeSupakorn <125183790+FlukeSupakorn@users.noreply.github.com>

---
+++ frontend/lib/features/home/ui/home_ui.dart
@@ -0,0 +1,75 @@
+// ui/home_ui.dart
+import 'package:flutter/material.dart';
+import 'package:fundflow/core/widgets/home/custom_text_button.dart';
+import 'package:fundflow/features/home/ui/bank_section.dart';
+import 'package:fundflow/features/home/ui/category_section.dart';
+import 'package:fundflow/features/home/ui/profile_section.dart';
+import 'package:fundflow/core/themes/app_styles.dart';
+
+class HomeUI extends StatelessWidget {
+  const HomeUI({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const ProfileSection(),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    "บัญชีธนาคาร",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  CustomTextButton(
+                    text: 'เพิ่มบัญชีธนาคาร +',
+                    color: AppColors.pink,
+                    fontSize: 12,
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/addBank');
+                    },
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10),
+              const BankSection(),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    "ประเภทค่าใช้จ่าย",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  CustomTextButton(
+                    text: 'เพิ่มประเภทค่าใช้จ่าย +',
+                    color: AppColors.pink,
+                    fontSize: 12,
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/addCategory');
+                    },
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10),
+              const CategorySection(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
