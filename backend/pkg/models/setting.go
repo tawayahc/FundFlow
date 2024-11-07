dc0cee9a1b6545c4579113b154045b96bdf4a15b
MawinS <mawin.sric@mail.kmutt.ac.th>
2024-11-07 15:41:08 +0700

    fix: add parameter password to delete-account path

--- backend/pkg/models/setting.go
+++ backend/pkg/models/setting.go
@@ -8,3 +8,7 @@ type ChangePasswordRequest struct {
 	OldPassword string `json:"old_password" binding:"required,min=8"`
 	NewPassword string `json:"new_password" binding:"required,min=8"`
 }
+
+type DeleteAccountRequest struct {
+	Password string `json:"password" binding:"required,min=8"`
+}
