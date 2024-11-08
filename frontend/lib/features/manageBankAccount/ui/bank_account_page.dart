c203280670fa06a482a70a65c9791057d547a3d5
NoCODEEE <97788979+NoCODEEE@users.noreply.github.com>
2024-11-08 22:08:12 +0700

    Frontend bank account (#12)
    * chore: Add const
    
    Add const and copy TabBar from transaction
    
    * chore: Add widget
    
    Add more const and incorporate new widget in BankAccountPage
    
    * chore: Add widget
    
    Add more const and incorporate new widget in BankAccountPage
    
    * feat: Add widget
    
    BankAccount and Category page functional UI and widget
    
    * chore: Move file and clean up messes
    
    Move transaction model into the Home folder for further work

--- frontend/lib/features/manageBankAccount/ui/bank_account_page.dart
+++ frontend/lib/features/manageBankAccount/ui/bank_account_page.dart
@@ -1,11 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:fundflow/features/manageBankAccount/ui/transaction_item.dart';
-import 'package:fundflow/features/manageBankAccount/ui/transaction_lists.dart';
 
 import '../../../core/themes/app_styles.dart';
+import '../../home/models/transaction.dart';
+import '../../home/repository/transaction_repository.dart';
 
-class BankAccountPage extends StatelessWidget {
+class BankAccountPage extends StatefulWidget {
   const BankAccountPage({super.key});
+
+  @override
+  _BankAccountPageState createState() => _BankAccountPageState();
+}
+
+class _BankAccountPageState extends State<BankAccountPage>
+    with SingleTickerProviderStateMixin {
+  String _type = 'income';
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+    _tabController.index = ['income', 'expense', 'transfer'].indexOf(_type);
+
+    _tabController.addListener(() {
+      setState(() {
+        _type = ['income', 'expense', 'transfer'][_tabController.index];
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  List<Transaction> get filteredTransactions {
+    if (_tabController.index == 0) {
+      // Show only income transactions
+      return transactions.where((transaction) => transaction.amount > 0).toList();
+    } else {
+      // Show only outcome transactions
+      return transactions.where((transaction) => transaction.amount < 0).toList();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +61,7 @@ class BankAccountPage extends StatelessWidget {
               },
             ),
             //---------- **รูปธนาคาร
-            Row(
+            const Row(
               children: [
                 // รูปธนาคาร
                 CircleAvatar(
@@ -47,16 +86,16 @@ class BankAccountPage extends StatelessWidget {
                 ),
               ],
             ),
-            SizedBox(height: 16),
+            const SizedBox(height: 16),
             //---------- **กล่องเงิน
             Container(
-              padding: EdgeInsets.fromLTRB(0, 8, 4, 0), // padding (left, top, right, bottom)
+              padding: const EdgeInsets.fromLTRB(0, 8, 4, 0), // padding (left, top, right, bottom)
               width: 296, // สุดขอบ
               decoration: BoxDecoration(
                 color: Colors.green,
                 borderRadius: BorderRadius.circular(12),
               ),
-              child: Column(
+              child: const Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Row(
@@ -95,27 +134,74 @@ class BankAccountPage extends StatelessWidget {
                 ],
               ),
             ),
-            SizedBox(height: 30),
-            Text(
+            const SizedBox(height: 30),
+            const Text(
               'ประวัติการทำรายการ',
               style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 4),
+            const SizedBox(height: 12),
             // Transaction ตรงนี้
+            Center(
+              child: PreferredSize(
+                preferredSize: const Size.fromHeight(40),
+                child: ClipRRect(
+                  borderRadius: const BorderRadius.all(Radius.circular(10)),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Container(
+                        height: 40,
+                        width: 200,
+                        margin: const EdgeInsets.symmetric(horizontal: 20),
+                        decoration: const BoxDecoration(
+                          borderRadius:
+                          BorderRadius.all(Radius.circular(10)),
+                          color: AppColors.primary,
+                        ),
+                        child: TabBar(
+                          controller: _tabController,
+                          indicatorSize: TabBarIndicatorSize.tab,
+                          dividerColor: Colors.transparent,
+                          indicator: const BoxDecoration(
+                            color: Colors.green,
+                            borderRadius:
+                            BorderRadius.all(Radius.circular(10)),
+                          ),
+                          labelColor: Colors.white,
+                          unselectedLabelColor: Colors.white,
+                          tabs: const [
+                            Tab(
+                              icon: Icon(Icons.download) ,
+                            ),
+                            Tab(
+                              icon: Icon(Icons.upload),
+                            ),
+                            Tab(
+                              icon: Icon(Icons.compare_arrows),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 12),
             Expanded(
               child: CustomScrollView(
                 slivers: [
                   SliverList(
                     delegate: SliverChildBuilderDelegate(
                           (context, index) {
-                        final transaction = transactions[index];
+                        final filteredItem = filteredTransactions[index];
                         return TransactionItem(
-                          amount: transaction.amount,
-                          category: transaction.category,
-                          type: transaction.type,
+                          amount: filteredItem.amount,
+                          category: filteredItem.category,
+                          type: filteredItem.memo,
                         );
                       },
-                      childCount: transactions.length,
+                      childCount: filteredTransactions.length,
                     ),
                   ),
                 ],
