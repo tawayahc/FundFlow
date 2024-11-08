178f4a09902f492b4702e2a731b32eeb21f15762
Mawin Srichat <mawin.sric@mail.kmutt.ac.th>
2024-11-08 20:57:42 +0700

    feat: bank endpoint

--- backend/pkg/controllers/bank_controller.go
+++ backend/pkg/controllers/bank_controller.go
@@ -59,3 +59,45 @@ func CreateBank(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{"message": "Bank created successfully"})
 }
+
+// Update a bank
+func UpdateBank(c *gin.Context) {
+	bankID, err := strconv.ParseUint(c.Param("bank_id"), 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid bank ID"})
+		return
+	}
+
+	var bank models.UpdateBankRequest
+	if err := c.ShouldBindJSON(&bank); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	claims, _ := utils.ExtractDataFromToken(c.GetHeader("Authorization"))
+
+	if err := services.UpdateBank(uint(bankID), bank.Name, bank.BankName, claims.UserID); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Bank updated successfully"})
+}
+
+// Delete a bank
+func DeleteBank(c *gin.Context) {
+	bankID, err := strconv.ParseUint(c.Param("bank_id"), 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid bank ID"})
+		return
+	}
+
+	claims, _ := utils.ExtractDataFromToken(c.GetHeader("Authorization"))
+
+	if err := services.DeleteBank(uint(bankID), claims.UserID); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Bank deleted successfully"})
+}
