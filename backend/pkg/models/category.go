40330cebb7399e2b8bb2cc4f20b3a3513976d95c
MawinS <mawin.sric@mail.kmutt.ac.th>
2024-11-08 12:24:45 +0700

    feat: complete category path

---
+++ backend/pkg/models/category.go
@@ -0,0 +1,36 @@
+package models
+
+import (
+	"gorm.io/gorm"
+)
+
+type Category struct {
+	gorm.Model
+	Name          string        `gorm:"size:50;not null"`
+	ColorCode     string        `gorm:"size:10;not null"`
+	Amount        float64       `gorm:"type:decimal(15,2);default:0.00"`
+	UserProfileID uint          `gorm:"not null"`
+	UserProfile   UserProfile   `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;foreignKey:UserProfileID;references:AuthID"`
+	Transactions  []Transaction `gorm:"foreignKey:CategoryID"`
+}
+
+type CreateCategoryRequest struct {
+	Name      string `json:"name" binding:"required"`
+	ColorCode string `json:"color_code" binding:"required"`
+}
+
+type DeleteCategoryRequest struct {
+	ID uint `json:"id" binding:"required"`
+}
+
+type UpdateCategoryRequest struct {
+	CategoryID   uint   `json:"category_id" binding:"required"`
+	NewName      string `json:"new_name" binding:"required"`
+	NewColorCode string `json:"new_color_code" binding:"required"`
+}
+
+type CategoryDTO struct {
+	ID        uint   `json:"id"`
+	Name      string `json:"name"`
+	ColorCode string `json:"color_code"`
+}
--- backend/pkg/services/category_service.go
+++ backend/pkg/services/category_service.go
@@ -22,3 +22,41 @@ func CreateCategory(categoryName string, colorCode string, userID uint) error {
 
 	return nil
 }
+
+// Get Categories retrieves all categories for a user
+func GetCategories(userID uint) ([]models.CategoryDTO, error) {
+	var categories []models.CategoryDTO
+	if err := config.DB.Table("categories").Select("id, name, color_code").Where("user_profile_id = ?", userID).Find(&categories).Error; err != nil {
+		return nil, errors.New("failed to retrieve categories")
+	}
+
+	return categories, nil
+}
+
+// Delete Category deletes a category
+func DeleteCategory(categoryID uint, userID uint) error {
+	var category models.Category
+	if err := config.DB.Where("id = ? AND user_profile_id = ?", categoryID, userID).First(&category).Error; err != nil {
+		return errors.New("category not found")
+	}
+
+	if err := config.DB.Unscoped().Delete(&category).Error; err != nil {
+		return errors.New("failed to delete category")
+	}
+
+	return nil
+}
+
+// UpdateCategory updates a category
+func UpdateCategory(categoryID uint, categoryName string, colorCode string, userID uint) error {
+	var category models.Category
+	if err := config.DB.Where("id = ? AND user_profile_id = ?", categoryID, userID).First(&category).Error; err != nil {
+		return errors.New("category not found")
+	}
+
+	if err := config.DB.Model(&category).Updates(models.Category{Name: categoryName, ColorCode: colorCode}).Error; err != nil {
+		return errors.New("failed to update category")
+	}
+
+	return nil
+}
